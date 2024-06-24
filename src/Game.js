1decc004036880615014b64211ba03e0a8c26d09
GrowthToBetter <baratrahjaga@gmail.com>
2024-06-25 05:45:27 +0700

    initialize init

---
+++ src/Game.js
@@ -0,0 +1,45 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC9NBGgCQNTcCeagvCoO1GS1QM2BE8KZ7k",
+  authDomain: "tabel-periodic.firebaseapp.com",
+  databaseURL:
+    "https://tabel-periodic-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "tabel-periodic",
+  storageBucket: "tabel-periodic.appspot.com",
+  messagingSenderId: "391430727013",
+  appId: "1:391430727013:web:552d5788757f7222fe6d8c",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+class Game {
+  constructor() {
+    this.container = document.querySelector(".container");
+  }
+  Login() {
+    signInWithPopup(auth, provider)
+      .catch((error) => {
+        alert(error)
+      });
+  }
+  init() {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+      } else {
+        let confirms = confirm("Anda belum login, Login sekarang?");
+        if (confirms) {
+          this.Login();
+        }
+      }
+    });
+  }
+}
+
+window.addEventListener("load", () => {
+  const Games = new Game();
+  Games.init();
+});
