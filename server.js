19d3d5ffa9356b238e135432cdd04fabe298f7a8
GrowthToBetter <baratrahjaga@gmail.com>
2024-07-23 11:19:07 +0700

    menambahkan fitur contact dan beberapa optimasi design

--- server.js
+++ server.js
@@ -3,13 +3,16 @@ const appExp= express()
 const port=3000
 
 appExp.use(express.static('dist'));
-
+appExp.use(express.static('dist/src'));
 appExp.set('view engine', 'ejs');
 appExp.get('/',(req,res)=>{
     res.render('../dist/index')
 })
 appExp.get('/Auth',(req,res)=>{
     res.render('../dist/Auth')
+})
+appExp.get('/Contact',(req,res)=>{
+    res.render('../dist/src/Contact')
 })
 
 appExp.listen(port,()=>{
